0282f7fdd845e8498786b98b76f729ff97c86711
Lena_Bokhan <leliklena@i.ua>
2025-09-17 12:07:29 +0300

    fix: clean code

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-//import cookieParser from 'cookie-parser';
+
 import dotenv from 'dotenv';
 import router from './routers/index.js';
 import { getEnvVar } from './utils/getEnvVar.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
-/*import { swaggerDocs } from './middlewares/swaggerDocs.js';*/
 
 dotenv.config();
 export default function setupServer() {
@@ -15,8 +14,7 @@ export default function setupServer() {
   app.use(express.json());
   app.use(pino({ transport: { target: 'pino-pretty' } }));
   app.use(cors());
-  /*app.use(cookieParser());
-  app.use('/api-docs', swaggerDocs());*/
+
   app.use(router);
   app.use(notFoundHandler);
   app.use(errorHandler);
